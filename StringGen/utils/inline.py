b81e9b74d5d05be9a2974bd083a94694b57fb879
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-05-09 00:11:57 +0200

    Add files via upload

---
+++ StringGen/utils/inline.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup,Message
+
+from config import SUPPORT_CHAT,OWNER_ID
+
+
+keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="بدا استخراج الجلسة 🖥️", callback_data="gensession")
+                    ],
+                    [
+                    InlineKeyboardButton("سـورس بشار - Mahmoud", url="t.me/czzwc")
+                    ],
+                    [
+                    InlineKeyboardButton("مـعـلـومات الـمـطـور", url="t.me/Syri20"),
+                ],
+                [
+                    InlineKeyboardButton("الـمـطـور👷", user_id=OWNER_ID),
+                    InlineKeyboardButton("اوامـر الـبـوت", url="https://telegra.ph/%D8%A7%D9%88%D8%A7%D9%85%D8%B1-%D8%A8%D9%88%D8%AA-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%AE%D8%B1%D8%A7%D8%AC-10-11")
+                ]
+            ]
+        )
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="بايروجورام v1", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="بايروجورام v2", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="تليثون", callback_data="telethon")],
+    ]
+)
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="استخرج مجدداً", callback_data="gensession")]]
+)
